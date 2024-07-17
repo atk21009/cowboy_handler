6d5bf136c27c073fe2d014f47fcaf0f76f021ea1
atk21009 <tja21009@byui.edu>
2024-07-17 10:12:10 -0600

    Remove the ping death

--- src/handlers/package_handler.erl
+++ src/handlers/package_handler.erl
@@ -10,31 +10,22 @@ init(Req0, Opts) ->
         error -> Data
     end,
 
-    % Ping logic@logic.taylor58.dev to establish connection
-    case net_adm:ping('logic@logic.taylor58.dev') of
-        pong ->
-            % If ping successful, make RPC call
-            case erpc:call('logic@logic.taylor58.dev', package_server, package, [Path, DecodedData]) of
-                {ok} ->
-                    %% If response is ok without data, reply with 200 OK and no body
-                    Req = cowboy_req:reply(200, #{<<"content-type">> => <<"application/json">>}, Req0),
-                    {ok, Req, Opts};
-                {ok, Response} ->
-                    ResponseData = jsx:encode(Response),
-                    Req = cowboy_req:reply(200, #{<<"content-type">> => <<"application/json">>}, ResponseData, Req0),
-                    {ok, Req, Opts};
-                {fail, Reason} ->
-                    ErrorResponse = jsx:encode(#{error => Reason}),
-                    Req = cowboy_req:reply(400, #{<<"content-type">> => <<"application/json">>}, ErrorResponse, Req0),
-                    {fail, Req, Opts};
-                {'EXIT', Reason} ->
-                    ErrorResponse = jsx:encode(#{error => erpc_failed, reason => Reason}),
-                    Req = cowboy_req:reply(500, #{<<"content-type">> => <<"application/json">>}, ErrorResponse, Req0),
-                    {fail, Req, Opts}
-            end;
-        pang ->
-            % If ping fails, handle the situation (e.g., log error, reply with error response)
-            ErrorResponse = jsx:encode(#{error => <<"Failed to ping logic node">>}),
+    % If ping successful, make RPC call
+    case erpc:call('logic@logic.taylor58.dev', package_server, package, [Path, DecodedData]) of
+        {ok} ->
+            %% If response is ok without data, reply with 200 OK and no body
+            Req = cowboy_req:reply(200, #{<<"content-type">> => <<"application/json">>}, Req0),
+            {ok, Req, Opts};
+        {ok, Response} ->
+            ResponseData = jsx:encode(Response),
+            Req = cowboy_req:reply(200, #{<<"content-type">> => <<"application/json">>}, ResponseData, Req0),
+            {ok, Req, Opts};
+        {fail, Reason} ->
+            ErrorResponse = jsx:encode(#{error => Reason}),
+            Req = cowboy_req:reply(400, #{<<"content-type">> => <<"application/json">>}, ErrorResponse, Req0),
+            {fail, Req, Opts};
+        {'EXIT', Reason} ->
+            ErrorResponse = jsx:encode(#{error => erpc_failed, reason => Reason}),
             Req = cowboy_req:reply(500, #{<<"content-type">> => <<"application/json">>}, ErrorResponse, Req0),
             {fail, Req, Opts}
     end.
